0a7e5f58b7585c9488c3641889a7f608572e5a53
joakin <joaquin@chimeces.com>
2017-11-01 18:09:28 +0100

    initial commit

---
+++ js/0.js
@@ -0,0 +1,17 @@
+webpackJsonp([0],{
+
+/***/ 3:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_app__ = __webpack_require__(2);
+
+
+/* harmony default export */ __webpack_exports__["default"] = (() => Object(__WEBPACK_IMPORTED_MODULE_0__components_app__["a" /* default */])("<h1>Home page</h1>"));
+
+
+/***/ })
+
+});
+//# sourceMappingURL=0.js.map
